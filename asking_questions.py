67427421e9275766d18b28985eb92b83084633e1
BEKIR MUGAYITOGLU <bmugayitoglu@gmail.com>
2019-12-16 21:13:44 -0800

    Add files via upload

---
+++ asking_questions.py
@@ -0,0 +1,31 @@
+# Logical operators
+
+# Prompt user to agree
+print("Type 'y' if you answer YES or 'n' for answer NO")
+s = input("Is it sunny today?\n")
+# Check whether yes or no
+if s =="Y" or s =="y":
+    print("Yes.")
+elif s =="N" or s =="n":
+    print("No.")
+
+s = input("Do your parents live in big city?\n")
+# Check whether yes or no
+if s =="Y" or s =="y":
+    print("Yes.")
+elif s =="N" or s =="n":
+    print("No.")
+
+s = input("Do you like to do homework?\n")
+# Check whether yes or no
+if s =="Y" or s =="y":
+    print("Yes.")
+elif s =="N" or s =="n":
+    print("No.")
+
+s = input("Do you like to play soccer?\n")
+# Check whether yes or no
+if s =="Y" or s =="y":
+    print("Yes.")
+elif s =="N" or s =="n":
+    print("No.")
